d33d16326bb2be4e49bbe8ab3cd94a66c91c32e5
papercrane1001 <wallison@westmont.edu>
2025-08-26 16:53:17 -0700

    ?

---
+++ 1.6/Source/AdvHeatsink/AdvHeatsink/AdvHeatsink.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvHeatsink
+{
+    public class AdvCoolingSystem:Rimatomics.CoolingTower
+    {
+        public override void Tick()
+        {
+            base.Tick();
+            if (CoolingNet.CoolingLoopRatio > 0f)
+            {
+                fuel?.Refuel(WaterUsage / 60f);
+                //fuel?.ConsumeFuel(WaterUsage / 60f);
+            }
+        }
+    }
+}
