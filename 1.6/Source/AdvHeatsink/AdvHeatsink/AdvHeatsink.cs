83ba6e70e035973325f02aec9aec4a5828e004c6
papercrane1001 <wallison@westmont.edu>
2026-03-26 17:25:01 -0700

    Updated for 1.6.  Finally noticed I had it pointing to the tower class instead of the radiator class.

--- 1.6/Source/AdvHeatsink/AdvHeatsink/AdvHeatsink.cs
+++ 1.6/Source/AdvHeatsink/AdvHeatsink/AdvHeatsink.cs
@@ -6,13 +6,14 @@ using System.Threading.Tasks;
 
 namespace AdvHeatsink
 {
-    public class AdvCoolingSystem:Rimatomics.CoolingTower
+    public class AdvCoolingSystem:Rimatomics.CoolingSystem
     {
         public override void Tick()
         {
             base.Tick();
             if (CoolingNet.CoolingLoopRatio > 0f)
             {
+                //Bit of a hacky way to remove excessive fuel use
                 fuel?.Refuel(WaterUsage / 60f);
                 //fuel?.ConsumeFuel(WaterUsage / 60f);
             }
